e3dee633ce82945616d381a0ad1f8c4c81adf20a
Tiago Rascazzi <tiago@rascazzi.com>
2019-02-04 09:28:38 -0500

    Use Utils for i2c setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2015,
- * National Instruments Corporation.
- * All rights reserved.
- */
-
 #include <vector>
 #include <iostream>
 
@@ -26,8 +20,6 @@ using namespace std;
 using namespace cv;
 
 #define ENABLE_SERVER 1
-
-NiFpga_Status setupI2CB(MyRio_I2c* i2c);
 
 extern NiFpga_Session myrio_session;
 NiFpga_Status status;
@@ -61,7 +53,7 @@ int main(int argc, char **argv) {
 	//clock_t myrioInit = clock();
 
 	MyRio_I2c i2c;
-	status = setupI2CB(&i2c);
+	status = Utils::setupI2CB(&myrio_session, &i2c);
 
 	Motor_Controller mc = Motor_Controller(&i2c);
 	mc.controllerEnable(DC);
@@ -343,59 +335,6 @@ int main(int argc, char **argv) {
 	mc.controllerReset(DC);
 
 	status = MyRio_Close();
-
-	return status;
-}
-
-
-
-
-NiFpga_Status setupI2CB(MyRio_I2c* i2c){
-	uint8_t selectReg;
-
-	/*
-	 * Initialize the I2C struct with registers from the FPGA personality.
-	 */
-	i2c->addr = NiFpga_MyRio1900Fpga60_ControlU8_I2CBADDR;//I2CAADDR;
-	i2c->cnfg = NiFpga_MyRio1900Fpga60_ControlU8_I2CBCNFG;//I2CBCNFG;
-	i2c->cntl = NiFpga_MyRio1900Fpga60_ControlU8_I2CBCNTL;//I2CBCNTL;
-	i2c->cntr = NiFpga_MyRio1900Fpga60_ControlU8_I2CBCNTR;//I2CBCNTR;
-	i2c->dati = NiFpga_MyRio1900Fpga60_IndicatorU8_I2CBDATI;//I2CBDATI;
-	i2c->dato = NiFpga_MyRio1900Fpga60_ControlU8_I2CBDATO;//I2CBDATO;
-	i2c->go = NiFpga_MyRio1900Fpga60_ControlBool_I2CBGO;//I2CBGO;
-	i2c->stat = NiFpga_MyRio1900Fpga60_IndicatorU8_I2CBSTAT;//I2CBSTAT;
-
-	/*
-	 * Enable the I2C functionality on Connector B
-	 * Read the value of the SELECTB register.
-	 */
-	status = NiFpga_ReadU8(myrio_session, NiFpga_MyRio1900Fpga60_ControlU8_SYSSELECTB, &selectReg);
-	MyRio_ReturnValueIfNotSuccess(status, status, "Could not read from the SYSSELECTB register!");
-
-	/*
-	 * Set bit7 of the SELECT register to enable the I2C functionality. The
-	 * functionality of this bit is specified in the documentation.
-	 */
-	selectReg = selectReg | (1 << 7);
-
-	/*
-	 * Write the updated value of the SELECT register.
-	 */
-	status = NiFpga_WriteU8(myrio_session, NiFpga_MyRio1900Fpga60_ControlU8_SYSSELECTB, selectReg);
-	MyRio_ReturnValueIfNotSuccess(status, status, "Could not write to the SYSSELECTB register!");
-
-	/*
-	 * Set the speed of the I2C block.
-	 *
-	 * Standard mode (100 kbps) = 213.
-	 * Fast mode (400 kbps) = 63.
-	 */
-	I2c_Counter(i2c, 213);
-
-	 /*
-	  * Enable the I2C block.
-	  */
-	I2c_Configure(i2c, I2c_Enabled);
 
 	return status;
 }
